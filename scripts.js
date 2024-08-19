b5dd4d4e4eea63bf45dcdf234200743114c9125a
mohitjangale8 <mohit.jangale23@spit.ac.in>
2024-08-19 09:58:10 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginButton = document.querySelector('.btn-login');
+    const signupButton = document.querySelector('.btn-signup');
+
+    loginButton.addEventListener('click', function() {
+        window.location.href = 'login.html';
+    });
+
+    signupButton.addEventListener('click', function() {
+        window.location.href = 'signup.html'; 
+    });
+});
 
